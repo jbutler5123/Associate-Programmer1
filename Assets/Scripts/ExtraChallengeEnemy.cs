836d7f95c138b6d96e15cd60f33fbef6785b5f65
John Butler <jbutler5123@gmail.com>
2022-09-30 18:44:36 -0400

    Extra challenge completed, second enemy patrol fixed.

--- Assets/Scripts/ExtraChallengeEnemy.cs
+++ Assets/Scripts/ExtraChallengeEnemy.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ExtraChallengeEnemy : MonoBehaviour
 {
-    /*
+    
     public Stats enemyStats;
 
     [Tooltip("The transform to which the enemy will pace back and forth to.")]
     public Transform[] patrolPoints;
 
-    private int currentPatrolPoint = 1.0f;
+    private int currentPatrolPoint = 1;
 
     /// <summary>
     /// Contains tunable parameters to tweak the enemy's movement.
@@ -21,34 +21,33 @@ public class ExtraChallengeEnemy : MonoBehaviour
         [Header("Enemy Settings")]
 
         [Tooltip("How fast the enemy moves.")]
-        public float speed =10 ;
+        public float speed;
 
         [Tooltip("Whether the enemy should move or not")]
         public bool move;
 
     }
-
-    void Update()
+    private void Update()
     {
         
         //if the enemy is allowed to move
-        if (enemyStats.move = true)
+        if (enemyStats.move == true)
         {
             Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
-            transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStatsspeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.speed * Time.deltaTime);
            
-            if (Vector3.Distance(transform.position, moveToPoint < 0.01f)
+            if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
             {
                 currentPatrolPoint++;
                 
-                if (currentPatrolPoint > patrolPoints.Length)
+                if (currentPatrolPoint >= patrolPoints.Length)
                 {
-                    currentPatrolPoint == 0;
+                    currentPatrolPoint = 0;
                 }
             
             }
         }
         
     }
-    */
+    
 }
