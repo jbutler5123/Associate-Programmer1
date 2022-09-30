7ea596361c8da2ca54fe73c853e1dd206fb00464
John Butler <jbutler5123@gmail.com>
2022-09-30 18:13:40 -0400

    Patrol Enemy fixed.

--- Assets/Scripts/PatrolEnemy.cs
+++ Assets/Scripts/PatrolEnemy.cs
@@ -24,28 +24,31 @@ public class PatrolEnemy : MonoBehaviour
 
     private int currentPatrolPoint = 0;
 
-    /*----------------------------BROKEN UPDATE LOOP---------------------------------------------------------------------------------
+    /*----------------------------BROKEN UPDATE LOOP---------------------------------------------------------------------------------*/
 
     private void Update()
     {
         if (enemyStats.move == true)
         {
-            Vector3 moveToPoint = patrolPoints(currentPatrolPoint).position;   
+            //Debug.Log($"Current patrol point is {currentPatrolPoint}");
+            Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;   
             transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.speed * Time.deltaTime);
 
             if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
             {
-                currentPatrolPoint +;
+                currentPatrolPoint++;
+                
 
-                if (currentPatrolPoint > patrolPoints.Length)  
+                if (currentPatrolPoint >= patrolPoints.Length)  
                 {
                     currentPatrolPoint = 0;
                 }
+                //Debug.Log($"Swapping to patrol point {currentPatrolPoint}");
             }
         }
     }
 
-       -------------------------------------------------------------------------------------------------------------------------------*/
+       /*-------------------------------------------------------------------------------------------------------------------------------*/
 }
 
 
